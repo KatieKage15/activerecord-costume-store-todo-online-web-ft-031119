8081a5ed04215ddc00bcd04f977520b0b1e6292a
victorious-messaging-4309 <wildayk@gmail.com>
2019-04-18 09:31:38 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,8 @@ ActiveRecord::Schema.define(version: 3) do
     t.float "price"
     t.string "image_url"
     t.string "size"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
