305e672eef3ff6e041ce975c08d5eb7f0f83b4a4
victorious-messaging-4309 <wildayk@gmail.com>
2019-04-18 09:23:52 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -14,7 +14,7 @@ class CreateCostumes < ActiveRecord::Migration[5.1]
       t.string :name
       t.float :price
       t.string :image_url
-      t.string :size 
+      t.string :size
     end
   end
 end
